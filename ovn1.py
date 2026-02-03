2958b616b14cc37f83aa07df7ed7c2ecdaa7355c
Eken <daniello@chalmers.se>
2026-02-03 15:13:28 +0100

    uppgifterna inskrivna

---
+++ ovn1.py
@@ -0,0 +1,8 @@
+'''
+Skapa en funktion f (P ) som tar en punkt P, och f¨orst reflekterar den i
+planet (genom origo) vinkelr¨att mot (1, 2, 2) och sedan i planet (genom
+origo) vinkelr¨att mot (−1, 3, −1).
+L˚at P = (1, 1, 1), och f (n)(P ) = f (f (· · · f (P ))), d.v.s. f applicerad n
+g˚anger p˚a P. Plotta punkterna P, f (P ), f (f (P )), . . . , f (n)(P ) (i en 3D-
+plot), d˚
+''' 
---
+++ ovn2.py
@@ -0,0 +1,10 @@
+'''''
+Skriv ett program som tar tre punkter P, Q, R i rummet och:
+• Ger ett felmeddelande om alla punkter ligger p˚a samma linje (d.v.s.
+inte sp¨anner upp ett plan).
+• Plottar upp triangeln med h¨orn i punkterna,
+• Ger en punkt och tv˚a vektorer som genererar planet,
+• Ger den normaliserade normalvektorn till planet och plottar den i
+mittenpunkten av triangeln (givet som 1
+3 (P + Q + R)). 
+'''''
