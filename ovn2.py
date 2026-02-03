bcac8c89ebf0181c48cb6d2ffa902959937bf99d
Eken <daniello@chalmers.se>
2026-02-03 16:29:13 +0100

    Update ovn2.py

--- ovn2.py
+++ ovn2.py
@@ -8,3 +8,21 @@ inte sp¨anner upp ett plan).
 mittenpunkten av triangeln (givet som 1
 3 (P + Q + R)). 
 '''''
+
+def threePoints (p, q, r):
+    # TODO se ifall alla punkter inte 'r pa samma linje 
+    # TODO se PLottar upp triangeln 
+    # TODO Ger en punkt och two vektorer som genererar planet 
+    # TODO ger den normaliserade normalvektorn enligt formeln i uppgiften  
+    
+    # (a,b,c) och (d,e,f) == a/d = b/e = c/f sa ar den paralell 
+    iterationObject = [p,q,r]
+
+    n = 1 
+    for i in iterationObject: 
+         
+        i / iterationObject[n]   # p / q 
+
+    pass  
+
+threePoints([1,1,2],[1,2,3],(-1,2,-1)) 
