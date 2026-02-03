0997c3d9cf9cfda98390d537ad97c4eef9634c24
Harry <harrydah29@gmail.com>
2026-02-03 17:05:39 +0100

    Update ovn2.py

--- ovn2.py
+++ ovn2.py
@@ -10,19 +10,46 @@ mittenpunkten av triangeln (givet som 1
 '''''
 
 def threePoints (p, q, r):
-    # TODO se ifall alla punkter inte 'r pa samma linje 
+    # TODO se ifall alla punkter inte är på samma linje 
     # TODO se PLottar upp triangeln 
     # TODO Ger en punkt och two vektorer som genererar planet 
     # TODO ger den normaliserade normalvektorn enligt formeln i uppgiften  
+    try:
+        # (a,b,c) och (d,e,f) == a/d = b/e = c/f sa ar den paralell 
+        iterationObject = [p,q,r]
+        
+        n = 1
+        diff = None
+        for i in iterationObject: # [ [1,1,2], [1,2,3], [-1,2,-1] ]
+            # i = [1,1,2]
+            for j in range(len(i)): 
+                if n == len(iterationObject):
+                    n = 0
+                    if diff == iterationObject[n][j] / i[j]:
+                        diff = iterationObject[n][j] / i[j]
+                    else:
+                        raise ValueError   
+                else: 
+                    diff = i[j] / iterationObject[n][j]
+
+                print(diff)
+            print(i, iterationObject[n])
+            n += 1
+            
+    except ValueError: 
+        print("Error: a/d = b/e = c/f -> inget plan")
+        # i / iterationObject[n]   # p / q 
     
-    # (a,b,c) och (d,e,f) == a/d = b/e = c/f sa ar den paralell 
-    iterationObject = [p,q,r]
+    #pass  #p[j] == q[j] or q[j] == r[j] or p[j] == r[j
+def plotTriangle():
+    pass
 
-    n = 1 
-    for i in iterationObject: 
-         
-        i / iterationObject[n]   # p / q 
+def generatePlane():
+    pass
 
-    pass  
+def normVector():
+    pass
 
-threePoints([1,1,2],[1,2,3],(-1,2,-1)) 
+
+
+threePoints([1,1,2],[1,2,3],[-1,2,-1]) 
