7c16be026a572a63c44730ee0d7b2caa8389a44e
Harry <harrydah29@gmail.com>
2026-02-12 15:22:34 +0100

    Update ovn1.py

--- lab2/ovn1.py
+++ lab2/ovn1.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def abc(A, v0, n):
-
     vk0 = np.matmul(A, v0)
     plt.plot(vk0)
+
     for k in range(n-1):
         vk0 = np.matmul(A, vk0)
         vk1 = vk0
         plt.plot(vk1)
+
     plt.show()
 
 A = np.array([[2,7],[3,9]])
