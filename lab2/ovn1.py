134ca69c70ae13b7069267263e8dd462a2aac6a4
Harry <harrydah29@gmail.com>
2026-02-12 14:32:38 +0100

    Update ovn1.py

--- lab2/ovn1.py
+++ lab2/ovn1.py
@@ -5,16 +5,12 @@ def abc(A, v0, n):
 
     vk0 = np.matmul(A, v0)
     plt.plot(vk0)
-
     for k in range(n-1):
         vk0 = np.matmul(A, vk0)
         vk1 = vk0
-
-        print(vk0)
         plt.plot(vk1)
-
     plt.show()
 
-A = np.array([[2,12],[4,3]])
-v0 = np.array([6, 7])
+A = np.array([[2,7],[3,9]])
+v0 = np.array([5, 9])
 abc(A, v0, 6)
