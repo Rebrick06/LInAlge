f0441eed64b54ac7bb78bb99f0f0f54c179e0527
Harry <harrydah29@gmail.com>
2026-02-12 13:56:22 +0100

    Update ovn1.py

--- lab2/ovn1.py
+++ lab2/ovn1.py
@@ -4,17 +4,20 @@ import numpy as np
 
 def abc(A, v0, n):
 
-    vk = A*v0
-    plt.plot(vk)
-    k = 1
+    vk0 = np.matmul(A, v0)
+    plt.plot(vk0)
     
-    while k < (n+1):
-        vk = A*vk
+    k = 1
+    while k < (n):
+        vk0 = np.matmul(A, vk0)
+
+        vk1 = vk0
+        print(vk0)
         k += 1
-        plt.plot(vk)
+
+        plt.plot(vk1)
     plt.show()
 
-
-A = np.array([[1,2],[3,4]])
-v0 = np.array([1, 2])
-abc(A, v0, 3)
+A = np.array([[2,7],[3,9]])
+v0 = np.array([5, 9])
+abc(A, v0, 6)
