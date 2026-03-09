84d39dd3f054705e738955815580796f73d74143
Eken <daniello@chalmers.se>
2026-03-09 15:12:01 +0100

    Final version

--- lab5/ovn1.py
+++ lab5/ovn1.py
@@ -2,18 +2,12 @@ import numpy as np
 import numpy.linalg as LA
 
 # Task 1 variables
-"""
 B = np.array([[9, 5], 
               [1, 5]])
-n = B.shape[0]
-b = np.random.rand(n)
-"""
 
 # Task 2 varaibles
 A = np.random.rand(500, 500)
 B_2 = A + A.T
-n = B_2.shape[0]
-b = np.random.rand(n)
 
 def randomVectorb(B): 
     n = B.shape[0] 
@@ -29,7 +23,8 @@ def Rayleight(B, b):
     return b.T @ B @ b
 
 # Function to calculate PowerIteration 
-def powerIteration(B, p):
+def powerIteration(B, p): 
+    b = randomVectorb(B)
     b_old = normalise(B @ b)
     rayl_old = Rayleight(B, b_old)
 
@@ -45,12 +40,10 @@ def powerIteration(B, p):
             rayl_old = rayl_new
 
 # Task 1 prints
-"""
-print(LA.eig(B))
-print(powerIteration(B, 10))
-"""
+print(f'Task 1 using LA.eig function = {LA.eig(B)}')
+print(f'Task 1 using our code = {powerIteration(B,10)}')
 
 # Task 2 prints 
 lamTask2, BTask2 = powerIteration(B_2, 10)
 task2Result = LA.norm((B_2 @ BTask2) - lamTask2 * BTask2  )  
-print(task2Result)       
+print(f'Task 2 result = {task2Result}')
